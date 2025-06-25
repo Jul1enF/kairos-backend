750238457a2ee880916545faabde82c5acf765b4
Jul1enF <j.furic@gmail.com>
2025-06-25 15:19:36 +0200

    réinitialisation cors app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// const session = require('express-session');
+// const passport = require('./config/auth');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const searchesRouter = require('./routes/searches');
-const session = require('express-session');
-const passport = require('./config/auth');
-
 const statusRouter = require('./routes/status');
 const resultsRouter = require('./routes/results')
 const dashboardRouter = require('./routes/dashboard')
@@ -18,39 +18,47 @@ const dashboardRouter = require('./routes/dashboard')
 var app = express();
 
 const cors = require('cors');
-app.use(cors({
-    // origin:  process.env.URL_FRONT,
-        origin:  "http://localhost:3001",
-    methods: 'GET,POST,PUT,DELETE', // méthodes HTTP autorisées
-    allowedHeaders: 'Content-Type,Authorization, ', // headers autorisés
-    credentials: true // Permet d'inclure les cookies dans les requêtes
-  }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true); 
-  
-  const allowedOrigins = [process.env.URL_FRONT, "https://play.google.com", "https://www.googleapis.com", "http://localhost:3001"];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+app.use(cors())
 
-  // res.setHeader('Access-Control-Allow-Origin', process.env.URL_FRONT); 
-  // your_frontend_domain, it's an example
-    next()
- });
-app.use(cookieParser());
-app.use(session({ secret: 'oui', resave: false, saveUninitialized: true,
-    cookie: {
-    secure: true, // Mettre à true en production si HTTPS est activé
-    httpOnly: true,
-    maxAge: 3600000 // Durée de vie du cookie en millisecondes (1 heure)
-  } }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(cors({
+//     origin:  process.env.URL_FRONT,
+//     methods: 'GET,POST,PUT,DELETE', // méthodes HTTP autorisées
+//     allowedHeaders: 'Content-Type,Authorization, ', // headers autorisés
+//     credentials: true // Permet d'inclure les cookies dans les requêtes
+//   }));
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true); 
+  
+//   const allowedOrigins = [process.env.URL_FRONT, "https://play.google.com", "https://www.googleapis.com"];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//        res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+
+//   // res.setHeader('Access-Control-Allow-Origin', process.env.URL_FRONT); 
+//   // your_frontend_domain, it's an example
+//     next()
+//  });
+// app.use(cookieParser());
+// app.use(session({ secret: 'oui', resave: false, saveUninitialized: true,
+//     cookie: {
+//     secure: true, // Mettre à true en production si HTTPS est activé
+//     httpOnly: true,
+//     maxAge: 3600000 // Durée de vie du cookie en millisecondes (1 heure)
+//   } }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/searches', searchesRouter);
