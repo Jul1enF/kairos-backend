8501b4976967420c08fab5cc029250d1d6cdb52c
Jul1enF <j.furic@gmail.com>
2025-06-25 15:25:23 +0200

    nouveau test requête sirene

--- routes/searches.js
+++ routes/searches.js
@@ -52,7 +52,7 @@ router.post('/newSearch', async (req, res) => {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
-      'Authorization': "61662980-6524-4f10-a629-8065241f108d",
+      'Authorization': "Bearer 61662980-6524-4f10-a629-8065241f108d",
     },
   })
   console.log("RESPONSE", response)
